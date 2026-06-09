bab4eed1ee4fd55ac3a21b2fe2afbf6a8cc2c75d
Jamon Bailey <83630529+jamon-bailey@users.noreply.github.com>
2026-06-09 08:41:52 -0400

    Demo Application

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,77 @@
 
-#include<iostream>
-#include"xptemp/main.hpp"
+#include <X11/Xlib.h>
 
+// Xlib (X11) HTML Documentation:
+// https://xorg.freedesktop.org/archive/current/doc/libX11/libX11/libX11.html
+//
+// Xlib (X11) PDF Documentation:
+// https://xorg.freedesktop.org/archive/current/doc/libX11/libX11/libX11.pdf
 
-int main(const int argc, const char* argv[]) {
-    const int value1 = 1;
-    const int value2 = 9;
+int main(const int argc, const char* argv[])
+{
+    // ---> Connect To Default Display Server <----------------------------------------------
+    Display* displayServer = XOpenDisplay(nullptr);
 
-    std::cout << "\nThe sum of " << value1 << " and " << value2 << " is:";
+    if (!displayServer)
+        return 1;
+    // ----------------------------------------------> Connect To Default Display Server <---
 
-    const int sum = myAdd(value1, value2);
+    // Index of the default screen on the display server
+    const int defaultScreen = XDefaultScreen(displayServer);
 
-    std::cout << '\n' << sum << std::endl;
+    // ---> Create Window On Display Server <------------------------------------------------
+    XSetWindowAttributes windowAttributes{};
 
-    return 0;
-}
+    windowAttributes.background_pixel = WhitePixel(displayServer, defaultScreen);
+    windowAttributes.border_pixel = BlackPixel(displayServer, defaultScreen);
 
-int myAdd(const int val1, const int val2) {
-    return val1+val2;
+    // Events this window will respond to
+    windowAttributes.event_mask = ExposureMask
+                                | StructureNotifyMask
+                                | KeyPressMask
+                                | KeyReleaseMask
+                                | ButtonPressMask
+                                | ButtonReleaseMask
+                                | PointerMotionMask;
+
+    Window mainWindowId = XCreateWindow( ///< Identifier of main application window
+        displayServer,                               ///< Display server
+        RootWindow(displayServer, defaultScreen),    ///< Parent window
+        0,                                           ///< Window x-coordinate
+        0,                                           ///< Window y-coordinate
+        800,                                         ///< Window width
+        500,                                         ///< Window height
+        1,                                           ///< Window border width
+        DefaultDepth(displayServer, defaultScreen),  ///< Window bits per pixel
+        InputOutput,                                 ///< Window classification
+        DefaultVisual(displayServer, defaultScreen), ///< Window pixel characteristics
+        CWBackPixel | CWBorderPixel | CWEventMask,   ///< Values set in attributes
+        &windowAttributes                            ///< Window attributes
+    );
+    // ------------------------------------------------> Create Window On Display Server <---
+
+    // Set window titlebar text
+    XStoreName(displayServer, mainWindowId, "Demo Xlib Application");
+
+    Atom wm_delete = XInternAtom(displayServer, "WM_DELETE_WINDOW", false);
+    XSetWMProtocols(displayServer, mainWindowId, &wm_delete, 1);
+
+    // Make newly created window visible on screen
+    XMapWindow(displayServer, mainWindowId);
+    
+    // ---> Run Window Event Loop <----------------------------------------------------------
+    XEvent event;
+
+    do {
+        XNextEvent(displayServer, &event);
+
+        if (event.type == ClientMessage && (Atom)event.xclient.data.l[0] == wm_delete)
+            break;
+    } while (true);
+    // ----------------------------------------------------------> Run Window Event Loop <---
+
+    XDestroyWindow(displayServer, mainWindowId);
+    XCloseDisplay(displayServer);
+
+    return 0; // End of application
 }
